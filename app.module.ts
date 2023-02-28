a07514d64b8dc42b5a6b0b8c689a25124dc6f17a
Paula Brinzea <paula.brinzea@treetop-medical.com>
2023-02-28 13:22:45 +0100

    initial commit

---
+++ app.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+
+import { AppComponent } from "./src/app/app.component";
+
+@NgModule({
+  imports: [BrowserModule, NgbModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
