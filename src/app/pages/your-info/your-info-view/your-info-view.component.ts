a71f3a599d581562ecb06264523b50acff0ab451
Paula Brinzea <paula.brinzea@treetop-medical.com>
2023-03-01 15:46:06 +0100

    design for first page

--- src/app/pages/your-info/your-info-view/your-info-view.component.ts
+++ src/app/pages/your-info/your-info-view/your-info-view.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-your-info-view',
-  templateUrl: './your-info-view.component.html',
-  styleUrls: ['./your-info-view.component.scss']
+  selector: "app-your-info-view",
+  templateUrl: "./your-info-view.component.html",
+  styleUrls: ["./your-info-view.component.scss"],
 })
 export class YourInfoViewComponent {
-
+  personalInfo = new FormGroup({
+    name: new FormControl("", Validators.required),
+    email: new FormControl("", [
+      Validators.required,
+      Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$"),
+    ]),
+    phone: new FormControl("", [
+      Validators.required,
+      Validators.pattern("[- +()0-9]+"),
+    ]),
+  });
 }
--- src/app/pages/your-info/your-info.module.ts
+++ src/app/pages/your-info/your-info.module.ts
@@ -1,17 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
 
-import { YourInfoRoutingModule } from './your-info-routing.module';
-import { YourInfoViewComponent } from './your-info-view/your-info-view.component';
-
+import { ReactiveFormsModule } from "@angular/forms";
+import { YourInfoRoutingModule } from "./your-info-routing.module";
+import { YourInfoViewComponent } from "./your-info-view/your-info-view.component";
 
 @NgModule({
-  declarations: [
-    YourInfoViewComponent
-  ],
-  imports: [
-    CommonModule,
-    YourInfoRoutingModule
-  ]
+  declarations: [YourInfoViewComponent],
+  imports: [CommonModule, YourInfoRoutingModule, ReactiveFormsModule],
 })
-export class YourInfoModule { }
+export class YourInfoModule {}
