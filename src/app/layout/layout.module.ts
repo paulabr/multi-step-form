9cd581c6a48b5f499e5c7e6c95c759ba921da7fc
Paula Brinzea <paula.brinzea@treetop-medical.com>
2023-03-02 17:35:12 +0100

    persist checked button

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { LayoutComponent } from "./layout.component";
+import { NavbarMobileModule } from "./navbar-mobile/navbar-mobile.module";
 import { NavbarModule } from "./navbar/navbar.module";
 
 @NgModule({
   declarations: [LayoutComponent],
-  imports: [CommonModule, RouterModule, NavbarModule],
+  imports: [CommonModule, RouterModule, NavbarModule, NavbarMobileModule],
 })
 export class LayoutModule {}
--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,8 +1,17 @@
 import { Component } from "@angular/core";
-
+import { Router } from "@angular/router";
+export enum Routes {
+  info = "/info",
+  plan = "/plan",
+  addons = "/add-ons",
+  summary = "/summary",
+}
 @Component({
   selector: "app-navbar",
   templateUrl: "./navbar.component.html",
   styleUrls: ["./navbar.component.scss"],
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  routes = Routes;
+  constructor(public router: Router) {}
+}
